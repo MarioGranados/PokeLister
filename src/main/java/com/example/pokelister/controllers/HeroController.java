e3852564f275a4f3adab44d0ff83b493ae083eda
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-22 16:19:27 -0500

    fixed issues

--- src/main/java/com/example/pokelister/controllers/HeroController.java
+++ src/main/java/com/example/pokelister/controllers/HeroController.java
@@ -1,12 +1,18 @@
 package com.example.pokelister.controllers;
 
+import com.example.pokelister.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HeroController {
+    private ProductRepository productsDao;
+
+    public HeroController(ProductRepository productsDao)  {this.productsDao = productsDao;}
     @GetMapping("/")
-    public String hero() {
+    public String hero(Model model) {
+        model.addAttribute("products", productsDao.findAll());
         return "index";
     }
 }
