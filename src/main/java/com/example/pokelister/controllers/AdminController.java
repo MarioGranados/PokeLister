724f2e5030b76aa5e9bf117fb1a7894518bcc327
Mario Granados <mario.alberto.granados1@gmail.com>
2022-04-24 19:13:21 -0500

    added seeder

--- src/main/java/com/example/pokelister/controllers/AdminController.java
+++ src/main/java/com/example/pokelister/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.pokelister.controllers;
 
 import com.example.pokelister.models.Product;
+import com.example.pokelister.repositories.CatagoryRepository;
 import com.example.pokelister.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AdminController {
 
     private ProductRepository productDao;
-    public AdminController(ProductRepository productDao) {
+    private CatagoryRepository catDao;
+
+    public AdminController(ProductRepository productDao, CatagoryRepository catDao) {
+        this.catDao = catDao;
         this.productDao = productDao;
     }
 
     @GetMapping("/admin")
     public String showAdminPage(Model model) {
         model.addAttribute("admin", true);
+        model.addAttribute("category", catDao.findAll());
         model.addAttribute("products", productDao.findAll());
         return "admin";
     }
@@ -30,7 +35,7 @@ public class AdminController {
 
     @PostMapping("/admin/create")
     public String createProduct(@RequestParam(name = "title") String title,
-                                @RequestParam (name = "description") String desc,
+                                @RequestParam(name = "description") String desc,
                                 @RequestParam(name = "price") long price,
                                 @RequestParam(name = "quantity") long quantity,
                                 @RequestParam(name = "image") String image,
@@ -45,8 +50,9 @@ public class AdminController {
         productDao.save(new Product(title, desc, price, quantity, image));
         return "admin";
     }
+
     @PostMapping("/admin/delete")
-    public String deleteProduct(@RequestParam (name = "product_id") long id) {
+    public String deleteProduct(@RequestParam(name = "product_id") long id) {
         System.out.println(id);
         productDao.deleteById(id);
         return "admin";
---
+++ src/main/resources/Seeder.sql
@@ -0,0 +1,34 @@
+INSERT INTO products (description, image, name, price, quantity)
+VALUES ('Most basic form of PokeBall used to catch pokemon',
+        'https://archives.bulbagarden.net/media/upload/7/79/Dream_Pok%C3%A9_Ball_Sprite.png',
+        'Poke-Ball', 200, 50),
+       ('Improved varient of regular PokeBall',
+        'https://archives.bulbagarden.net/media/upload/b/bf/Dream_Great_Ball_Sprite.png',
+        'Great-Ball', 600, 50),
+       ('Improved varient of Great-Ball used to catch pokemon',
+        'https://archives.bulbagarden.net/media/upload/a/a8/Dream_Ultra_Ball_Sprite.png',
+        'Ultra-Ball', 1200, 50),
+       ('Basic potion that Heals wounded Pokemon',
+        'https://archives.bulbagarden.net/media/upload/d/df/Dream_Potion_Sprite.png',
+        'Potion', 300, 50),
+       ('Improved potion from regular potion',
+        'https://archives.bulbagarden.net/media/upload/5/57/Dream_Super_Potion_Sprite.png',
+        'Super Potion', 700, 50),
+       ('Improved varient of regular potion',
+        'https://archives.bulbagarden.net/media/upload/b/bf/Dream_Great_Ball_Sprite.png',
+        'Super-Potion', 700, 50),
+       ('Improved varient of regular potion and super potion used to heal pokemon',
+        'https://archives.bulbagarden.net/media/upload/c/c8/Dream_Hyper_Potion_Sprite.png',
+        'Hyper-Potion', 1200, 50),
+       ('Revives fainted Pokemon',
+        'https://archives.bulbagarden.net/media/upload/8/8c/Dream_Revive_Sprite.png',
+        'Revive', 1500, 50);
+
+
+
+
+INSERT INTO catagories (catagory_name)
+VALUES ('PokeBalls'),
+  ('Potions'),
+  ('Berries');
+
